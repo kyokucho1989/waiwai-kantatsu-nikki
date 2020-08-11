750d28e87edda0bf205a364eba95a9c795d81ae1
ko <kyokucho1989@gmail.com>
2020-08-12 05:40:17 +0900

    データベースに追加できないエラー発生/質問用にpush

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -3,22 +3,30 @@ class PlantsController < ApplicationController
     @plants = Plant.all
 
     @user = User.find(params[:user_id])
-    @plant = @user.plants
+    @plants = @user.plants
+    #binding.pry
   end
   
   def new
     @user = User.find(params[:user_id])
     @plant = Plant.new
+    #binding.pry
   end
 
   def create
-    @plant = Plant.new(params[:plant])
-    #@entry = Entry.new(params[:entry])
+    @plant = Plant.new(plant_params)
+    Plant.create(plant_params) 
+    #binding.pry
   end
 
   def show
     @user = User.find(params[:user_id])
     @plant = @user.plants.find(params[:id])
+  end
+
+  private
+  def plant_params
+    params.require(:plant).permit(:user_id, :plant_name, :img)
   end
 
 end

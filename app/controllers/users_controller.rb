92958cc1a23616f95215c8da508d91c8d1c65c33
ko <kyokucho1989@gmail.com>
2020-09-24 22:45:47 +0900

    ユーザーレコード作成のテストまで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,22 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    # binding.pry
   end
 
   def new
     @user = User.new
-    #binding.pry
+    # binding.pry
   end
 
   def create
+    # binding.pry
     User.create!(user_params)    
   end
 
   def show
     @user = User.find(params[:id])
-    #binding.pry
+    # binding.pry
   end
 
   private
